3ef9973355f028947675894a1745d5326d8844fc
nikhil parate <nikhilparate@gmail.com>
2025-08-25 12:40:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import Weather from "./Components/Weather";
+import "./App.css";
+const App = () => {
+  const [allvalues, setvalues] = useState({
+    city: "nagpur",
+    location: {},
+    current: {},
+    error: "",
+    loading: false,
+  });
+  const fetchweather = async () => {
+    if (!allvalues.city) {
+      setvalues({
+        ...allvalues,
+        error: "Please enter a city name",
+        loading: false,
+      });
+      return;
+    }
+    setvalues({ ...allvalues, loading: true, error: "" });
+
+    const api = `https://api.weatherapi.com/v1/current.json?key=231f1c78815f44e4ade42427252508&q=${allvalues.city}&aqi=yes`;
+    const options = {
+      method: "Get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(api, options);
+      const data = await response.json();
+      if (data.error) {
+        setvalues({
+          ...allvalues,
+          city: "",
+          location: {},
+          current: {},
+          error: data.error.message,
+          loading: false,
+        });
+      } else {
+        setvalues({
+          ...allvalues,
+          location: data.location,
+          current: data.current,
+          error: "",
+          loading: false,
+        });
+      }
+    } catch (error) {
+      setvalues({
+        ...allvalues,
+        location: {},
+        current: {},
+        error: "Something went wrong. Please try again.",
+        loading: false,
+      });
+    }
+  };
+  useEffect(() => {
+    fetchweather();
+  }, []);
+  return (
+    <>
+      <div className="container-fluid weather-app d-flex flex-column align-items-center justify-content-center min-vh-100">
+        <div className="card shadow-lg bg-dark text-white p-4">
+          <div className="text-center mb-4">
+            <TiWeatherPartlySunny className="text-warning" size={50} />
+            <h1 className="fw-bold text-primary">Weather Forecast</h1>
+          </div>
+
+          <div className=" flex-column flex-md-row gap-2 mb-3">
+            <input
+              type="text"
+              className="form-control mb-3"
+              value={allvalues.city}
+              onChange={(e) =>
+                setvalues({ ...allvalues, city: e.target.value })
+              }
+              placeholder="Enter city name"
+            />
+            <button className="btn btn-primary w-100 " onClick={fetchweather}>
+              Get Weather
+            </button>
+          </div>
+
+          <p className="text-danger">{allvalues.error}</p>
+          
+          {
+          allvalues.loading ? (
+            <div className="spinner-border text-primary mx-auto" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          ) : allvalues.location.name ? (
+            <Weather
+              weatherdetails={{
+                location: allvalues.location,
+                current: allvalues.current,
+              }}
+            />
+          ) : 
+          null
+          }
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
---
+++ src/Components/Weather/index.jsx
@@ -0,0 +1,32 @@
+import { CiTempHigh } from "react-icons/ci";
+import { WiStrongWind, WiHumidity, WiCloud } from "react-icons/wi";
+const Weather=({weatherdetails})=>{
+    const{location,current}=weatherdetails;
+    return(
+    <div>
+        <h2>{location.name},{location.country}</h2>
+      <br />
+
+       <div className="d-flex align-items-center mb-2">
+        <CiTempHigh style={{ fontSize: "30px", marginRight: "8px" }} />
+        <p className="mb-0">Temp: {current.temp_c}°C</p>
+      </div>
+
+       <div className="d-flex align-items-center mb-2">
+        <WiStrongWind style={{ fontSize: "30px", marginRight: "10px", color: "skyblue" }} />
+        <p className="mb-0">Wind: {current.wind_kph} kph</p>
+      </div>
+
+      <div className="d-flex align-items-center mb-2">
+        <WiHumidity style={{ fontSize: "30px", marginRight: "10px", color: "blue" }} />
+        <p className="mb-0">Humidity: {current.humidity}%</p>
+      </div>
+
+      <div className="d-flex align-items-center mb-2">
+        <WiCloud style={{ fontSize: "30px", marginRight: "10px", color: "gray" }} />
+        <p className="mb-0">Condition: {current.condition.text}</p>
+      </div>
+        </div>
+    )
+}
+export default Weather
